c52ad2f9ae9bff5a57a25a3fb324e957dd7cf1fa
Ian Henderson <ianwhen@gmail.com>
2014-08-28 11:04:53 -0700

    Added simple search filtering

--- public/edit/edit.js
+++ public/edit/edit.js
@@ -1,7 +1,11 @@
 angular.module('app.edit', ['app.facts'])
 .controller('editCtrl', function($scope, data){
+  
+  $scope.getAllFacts = data.getAllFacts;
+
   $scope.addFact = function(){
     data.addFact($scope.fact);
+    $scope.fact = '';
   };
-  $scope.getAllFacts = data.getAllFacts;
+
 });
