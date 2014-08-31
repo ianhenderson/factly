2f9ee8ce1ccc4ac8ecbd149b79d27466bea26b8d
Ian Henderson <ianwhen@gmail.com>
2014-08-31 14:42:56 -0700

    Added a few comments

--- public/study/study.js
+++ public/study/study.js
@@ -2,6 +2,6 @@ angular.module('app.study', ['app.facts'])
 .controller('studyCtrl', function($scope, data){
   $scope.currentFact = "Click the button below to study your facts.";
   $scope.getNextFact = function(){
-    $scope.currentFact = data.getNextFact();
+    $scope.currentFact = data.getNextFact().fact;
   };
 });
