7815a6421e9ddffb7580796788d215efbceacb5f
Ian Henderson <ianwhen@gmail.com>
2014-08-30 19:45:37 -0700

    Added basic clientside POST functionality

--- public/login/login.js
+++ public/login/login.js
@@ -1,6 +1,23 @@
 angular.module('app.login', [])
-.controller(function($scope){
+.controller('loginCtrl', function($scope, $http){
 
-  $scope.loginUser = function(){};
+
+  $scope.loginUser = function(){
+    var params = {
+      method: 'POST',
+      url: 'api/login',
+      data: {
+        name: $scope.userName,
+        password: $scope.userPassword
+      }
+    };
+    $http(params)
+      .success(function(data, status){
+        console.log(status, data);
+      })
+      .error(function(data, status){
+        console.log(status, data);
+      });
+  };
 
 });
