97e29c5ebd6db39dc338920e6a6156c8e51317c1
Ian Henderson <ianwhen@gmail.com>
2015-08-14 10:10:50 -0700

    Add gzip compression, increase upload limit to 2mb, set cookie maxAge

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var checkSession = require('./server/checkSession');
+var compression = require('compression');
 var app = express();
 var port = process.env.PORT || 3000;
 
-// require('./server/build')();
+app.use(compression());
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '2mb'}));
 app.use(cookieParser());
 app.use('/api', checkSession);
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public', {
+  maxAge: 2592000000
+}));
 
 require('./server/routes.js')(app);
 app.listen(port);
