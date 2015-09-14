1f4ae59f573fb41ac737cee1080f08e1ed70a601
Ian Henderson <ianwhen@gmail.com>
2015-09-14 00:28:11 -0700

    Move route handling from routes.js into controller.js

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,137 @@
-// var db = require('./db.js');
-// var Promise = require('bluebird');
+var db = require('./db.js');
+var Promise = require('bluebird');
 
-module.exports = {};
+function login(req, res){
+
+  var c = req.cookies;
+  if (c && c.session && c.session.id) {
+    res.status(200).send('Already logged in as ' + c.session.name);
+    return;
+  }
+
+  if (!req.body.username || !req.body.password){
+
+    res.status(400).send("Error: POST must include a name and password.");
+
+  } else {
+
+    var name = req.body.username;
+    var password = req.body.password;
+
+    db.checkUser_(name, password)
+    .then(function(session){
+
+      if (session.data){
+
+        res.cookie('session', session.data, {httpOnly: true});
+        res.status(200).send(session.data);
+
+      } else {
+
+        res.status(403).send("Error: username / password incorrect.");
+
+      }
+
+    });
+
+  }
+}
+
+function logout(req, res){
+  res.clearCookie('session');
+  res.status(200).send('User signed out.');
+}
+
+function signup(req, res){
+
+  if (!req.body.username || !req.body.password){
+
+    res.status(400).send("Error: POST must include a username and password.");
+
+  } else {
+
+    var username = req.body.username;
+    var password = req.body.password;
+
+    db.checkUser_(username, password)
+      .then(function(user){
+
+        if (user.exists){
+
+          res.status(409).send("User already in database.");
+
+        } else {
+
+          db.addNewUser_(username, password)
+            .then(function(user){
+
+              res.status(201).send(user);
+              
+            });
+
+        }
+
+      });
+
+  }
+}
+
+function getKanji(req, res){
+
+  var c = req.cookies;
+  var id = c.session.id;
+
+  db.getNextFromQueue_(id)
+    .then(function(kanji){
+
+      if (kanji) {
+        res.status(200).send(kanji);
+      } else {
+        res.status(404).send("Nothing more to study.");
+      }
+
+    });
+}
+
+function getFacts(req, res){
+
+  var c = req.cookies;
+  var id = c.session.id;
+
+  db.getFacts_(id)
+    .then(function(rows){
+
+      res.status(200).send(rows);
+
+    });
+}
+
+function addFact(req, res){
+
+  if (!req.body.fact){
+
+    res.status(400).send("Error: POST must include a fact.");
+
+  } else {
+
+    var c = req.cookies;
+    var id = c.session.id;
+    var name = c.session.name;
+    var fact = req.body.fact;
+
+    db.addWord_2(id, fact)
+      .then(function(added){
+        res.status(201).send(["Success! Fact added to ", name, "'s collection: ", fact].join(''));
+      });
+
+  }
+}
+
+module.exports = {
+  login: login,
+  logout: logout,
+  signup: signup,
+  getKanji: getKanji,
+  getFacts: getFacts,
+  addFact: addFact
+};
--- server/middleware.js
+++ server/middleware.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
-var checkSession = require('./checkSession');
 var compression = require('compression');
 
 module.exports = function(app){
@@ -9,7 +8,6 @@ module.exports = function(app){
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json({limit: '2mb'}));
   app.use(cookieParser());
-  app.use('/api', checkSession);
   app.use(express.static(__dirname + '/../public', {
     maxAge: 2592000000
   }));
