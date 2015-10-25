8f36b8cbf4e2a2027b4ecd4558bcbe9ffe2698d4
Ian Henderson <ianwhen@gmail.com>
2015-10-25 13:26:27 -0700

    Add routing for alternate frontends

--- build.js
+++ build.js
@@ -6,75 +6,118 @@ var uglifyJS = require("uglify-js");
 var env = process.env.NODE_ENV ? 'prod' : 'dev';
 
 
-function buildPath(filename){
-  var path = ['public', 'angular-mat-design', 'dist'];
+function buildPath(subdir, filename){
+  subdir = subdir || 'angular-mat-design';
+  var path = ['public', subdir, 'dist'];
   if (filename) { path.push(filename); }
   return path.join('/');
 }
 
 var source = {
-  js: {
-    src: [
-      "public/angular-mat-design/js/app.js",
-    ],
-    vendor: {
-      dev: [
-        "node_modules/angular/angular.js",
-        "node_modules/angular-ui-router/release/angular-ui-router.js",
-        "node_modules/angular-animate/angular-animate.js",
-        "node_modules/angular-aria/angular-aria.js",
-        "node_modules/angular-material/angular-material.js",
-        "node_modules/angular-sanitize/angular-sanitize.js",
+  'angular-mat-design': {
+    js: {
+      src: [
+        "public/angular-mat-design/js/app.js",
       ],
-      prod: [
-        "node_modules/angular/angular.min.js",
-        "node_modules/angular-ui-router/release/angular-ui-router.min.js",
-        "node_modules/angular-animate/angular-animate.min.js",
-        "node_modules/angular-aria/angular-aria.min.js",
-        "node_modules/angular-material/angular-material.min.js",
-        "node_modules/angular-sanitize/angular-sanitize.min.js",
-      ]
+      vendor: {
+        dev: [
+          "node_modules/angular/angular.js",
+          "node_modules/angular-ui-router/release/angular-ui-router.js",
+          "node_modules/angular-animate/angular-animate.js",
+          "node_modules/angular-aria/angular-aria.js",
+          "node_modules/angular-material/angular-material.js",
+          "node_modules/angular-sanitize/angular-sanitize.js",
+        ],
+        prod: [
+          "node_modules/angular/angular.min.js",
+          "node_modules/angular-ui-router/release/angular-ui-router.min.js",
+          "node_modules/angular-animate/angular-animate.min.js",
+          "node_modules/angular-aria/angular-aria.min.js",
+          "node_modules/angular-material/angular-material.min.js",
+          "node_modules/angular-sanitize/angular-sanitize.min.js",
+        ]
+      }
+    },
+    css: {
+      src: [
+        "public/angular-mat-design/styles/style.css",
+        ],
+      vendor: {
+        dev: [
+          "node_modules/angular-material/angular-material.css",
+        ],
+        prod: [
+          "node_modules/angular-material/angular-material.min.css",
+        ]
+      }
     }
   },
-  css: {
-    src: [
-      "public/angular-mat-design/styles/style.css",
+  'angular': {
+    js: {
+      src: [
+        "public/angular/js/app.js",
       ],
-    vendor: {
-      dev: [
-        "node_modules/angular-material/angular-material.css",
-      ],
-      prod: [
-        "node_modules/angular-material/angular-material.min.css",
-      ]
+      vendor: {
+        dev: [
+          "node_modules/angular/angular.js",
+          "node_modules/angular-ui-router/release/angular-ui-router.js",
+          "node_modules/angular-animate/angular-animate.js",
+          "node_modules/angular-aria/angular-aria.js",
+          "node_modules/angular-material/angular-material.js",
+          "node_modules/angular-sanitize/angular-sanitize.js",
+        ],
+        prod: [
+          "node_modules/angular/angular.min.js",
+          "node_modules/angular-ui-router/release/angular-ui-router.min.js",
+          "node_modules/angular-animate/angular-animate.min.js",
+          "node_modules/angular-aria/angular-aria.min.js",
+          "node_modules/angular-material/angular-material.min.js",
+          "node_modules/angular-sanitize/angular-sanitize.min.js",
+        ]
+      }
+    },
+    css: {
+      src: [
+        "public/angular/styles/style.css",
+        ],
+      vendor: {
+        dev: [
+          "node_modules/angular-material/angular-material.css",
+        ],
+        prod: [
+          "node_modules/angular-material/angular-material.min.css",
+        ]
+      }
     }
   }
 };
 // make path directory
 console.time('build.js');
-mkdir('-p', buildPath());
+for (var key in source) {
+  mkdir('-p', buildPath(key));
 
-// concat source js > public/dist/main.js
-var src = cat(source.js.src);
+  // concat source js > public/dist/main.js
+  var src = cat(source[key].js.src);
 
-  // ngAnnotate
-  src = ngAnnotate(src, {add: true, single_quotes: true}).src;
+    // ngAnnotate
+    src = ngAnnotate(src, {add: true, single_quotes: true}).src;
 
-  // minify
-  if (env === 'prod') {
-    // console.log('* minifying...');
-    src = uglifyJS.minify(src, {fromString: true}).code;
-  }
+    // minify
+    if (env === 'prod') {
+      // console.log('* minifying...');
+      src = uglifyJS.minify(src, {fromString: true}).code;
+    }
 
-src.to(buildPath('main.js'));
+  src.to(buildPath(key, 'main.js'));
 
-// concat source js > public/dist/vendor.js
-cat(source.js.vendor[env]).to(buildPath('vendor.js'));
+  // concat source js > public/dist/vendor.js
+  cat(source[key].js.vendor[env]).to(buildPath(key, 'vendor.js'));
 
-// concat source css > public/dist/main.css
-cat(source.css.src).to(buildPath('main.css'));
+  // concat source css > public/dist/main.css
+  cat(source[key].css.src).to(buildPath(key, 'main.css'));
 
-// concat source css > public/dist/vendor.css
-cat(source.css.vendor[env]).to(buildPath('vendor.css'));
+  // concat source css > public/dist/vendor.css
+  cat(source[key].css.vendor[env]).to(buildPath(key, 'vendor.css'));
 
+}
 console.timeEnd('build.js');
--- server/middleware.js
+++ server/middleware.js
@@ -8,10 +8,13 @@ module.exports = function(app){
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json({limit: '2mb'}));
   app.use(cookieParser());
+  // Default frontend app: angular-material design
   app.use(express.static(__dirname + '/../public/angular-mat-design', {
     maxAge: 2592000000
   }));
-  app.use(express.static(__dirname + '/../public/react', {
+  // Alternate apps: /angular, /react, etc.
+  // Assets defined in build.js
+  app.use(express.static(__dirname + '/../public', {
     maxAge: 2592000000
   }));
 };
