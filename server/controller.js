291ea20544a75a7c8ebf39d227ebab27ad427aea
Ian Henderson <ianwhen@gmail.com>
2015-12-21 15:30:10 -0800

    Reduce all that dead whitespace

--- server/controller.js
+++ server/controller.js
@@ -1,38 +1,25 @@
 var db = require('./db.js');
 
 function login(req, res){
-
   var c = req.cookies;
   var username = req.body.username;
   var password = req.body.password;
-
   if (c && c.session && c.session.id) {
     res.status(200).send('Already logged in as ' + c.session.name);
     return;
   }
-
   if (!username || !password){
-
     res.status(400).send("Error: POST must include a name and password.");
-
   } else {
-
     db.checkUser(username, password)
     .then(function(session){
-
       if (session.data){
-
         res.cookie('session', session.data, {httpOnly: true});
         res.status(200).send(session.data);
-
       } else {
-
         res.status(403).send("Error: username / password incorrect.");
-
       }
-
     });
-
   }
 }
 
@@ -42,46 +29,30 @@ function logout(req, res){
 }
 
 function signup(req, res){
-
   var username = req.body.username;
   var password = req.body.password;
-
   if (!username || !password){
-
     res.status(400).send("Error: POST must include a username and password.");
-
   } else {
-
     db.checkUser(username, password)
       .then(function(user){
-
         if (user.exists){
-
           res.status(409).send("User already in database.");
-
         } else {
-
           db.addNewUser(username, password)
             .then(function(user){
-
               res.status(201).send(user);
-              
             });
         }
-
       });
-
   }
 }
 
 function getKanji(req, res){
-
   var c = req.cookies;
   var id = c.session.id;
-
   db.getNextFromQueue_(id)
     .then(function(kanji){
-
       if (kanji) {
         db.getSeenWordsRelatedToKanji(id, kanji)
           .then(function(words){
@@ -94,59 +65,42 @@ function getKanji(req, res){
       } else {
         res.status(404).send("Nothing more to study.");
       }
-
     });
 }
 
 function getFacts(req, res){
-
   var c = req.cookies;
   var id = c.session.id;
-
   db.getFacts_(id)
     .then(function(rows){
-
       res.status(200).send(rows);
-
     });
 }
 
 function addFact(req, res){
-
   var c = req.cookies;
   var id = c.session.id;
   var name = c.session.name;
   var fact = req.body.fact;
-
   if (!fact){
-
     res.status(400).send("Error: POST must include a fact.");
-
   } else {
-
     if (!Array.isArray(fact)) { fact = [fact] }
-
     db.addWords(id, fact)
       .then(function(added){
         res.status(201).send(["Success! Fact added to ", name, "'s collection: ", fact].join(''));
       });
-
   }
 }
 
 function getRelatedWords(req, res){
-
   var c = req.cookies;
   var id = c.session.id;
   var kanji = req.body.kanji;
-
   db.getSeenWordsRelatedToKanji(kanji, id)
     .then(function(rows){
-
       res.status(200).send(rows);
-
     });
-
 }
 
 module.exports = {
